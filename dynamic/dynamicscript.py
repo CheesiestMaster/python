2b591dc2a98c8de1c48ae58c05ac1c72aafcb4d3
CheesiestMaster <74749672+CheesiestMaster@users.noreply.github.com>
2022-01-20 08:40:17 -0500

    Update dynamicscript.py

--- dynamic/dynamicscript.py
+++ dynamic/dynamicscript.py
@@ -1,23 +1,30 @@
 # Dynamic Open Source Python Program
+
+#Import Modules
 import requests
 from os import getcwd
-owner = ""
-#owner = input("Who's Repo should I use (Leave blank for default): ")
+
+# User Input
+owner = input("Who's Repo should I use (Leave blank for default): ")
 if owner == "":
     owner = "CheesiestMaster"
 else:
 	print("Origin Switched to: "+owner)
 	print("I am not responsible for if you download a virus from a non default repository")
-project = "coterminal-deg"
-#project = input("Target program name: ")
+project = input("Target program name: ")
 while project == "":
     project = input("Target program name: ")
+
+#Networking
 url = "http://raw.github.com/"+owner+"/python/main/dynamic/"+project+".py"
-directory = getcwd()
-filename = directory + r"/dynamictmp.py"
 r=requests.get(url)
-import io
-f=io.open(filename, 'w', encoding="utf-8")
+
+#File Manipulation
+directory = getcwd()
+filename = directory + "/dynamictmp.py"
+f=open(filename, 'w')
 f.write(r.text)
 f.close()
+
+# Execute temporary file
 import dynamictmp
